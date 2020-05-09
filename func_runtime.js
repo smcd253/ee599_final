73230b08eee146ad148f6f97f3ae4c06ff9e2e25
Sam Bruner <sambruner1@gmail.com>
2020-05-09 16:35:37 -0700

    added promise to run

--- func_runtime.js
+++ func_runtime.js
@@ -1,27 +1,43 @@
 // find the number of 
 
-function a(max_val)  {
+const { PerformanceObserver, performance } = require('perf_hooks');
+
+
+async function a(max_val) {
     for (let i = 0; i < max_val; i++) {
-        
+
     }
 }
 
-let ffs = [a(12000), a(999999999), a(400)]; 
+//let ffs = [a(12000), a(999999999), a(400)]; 
+let parameters = [400, 999999999, 12000];
 
-async function time_funcs(functions) {
-    let ts = new Array(3); 
-    //for(f of functions) {
-     try{   let t1 = performance.now(); 
-        await functions[0]; 
-        let t2 = performance.now(); 
-        return t2-t1; 
-        //ts.push(t2-t1); 
-     }
-     catch (error){
-        console.log(error); 
-     }
-     //}
-    return ts; 
+async function time_funcs(input) {
+    let ts = [];
+    var i = 0;
+    for (f of input) {
+
+        try {
+            console.log("f: ", f);
+            let t1 = performance.now();
+            await a(f);
+            let t2 = performance.now();
+
+            ts.push(t2 - t1);
+
+            console.log(`time${i}: `, t2 - t1);
+            //return t2 - t1;
+            i++;
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+    return ts;
 }
 
-console.log('time: ', time_funcs(ffs)); 
+
+let output = time_funcs(parameters);
+output.then(function (result) {
+    console.log("output: ", result);
+})
