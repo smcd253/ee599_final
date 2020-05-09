0eaaca4edb82040a60f43dbbd021f72e77e1b1f1
Charles Andre <candre@Charless-MacBook-Pro-2.local>
2020-05-09 12:09:45 -0700

    tab fib

--- tabulation/fibonacci.js
+++ tabulation/fibonacci.js
@@ -1,7 +1,9 @@
 
-function fib_memo(n) {
+// source: https://miniwebtool.com/list-of-fibonacci-numbers/?number=20
+
+function fib_tab(n) {
     let mem = new Array(n+1); 
-    mem[0] = 1;
+    mem[0] = 0;
     mem[1] = 1;
     for(let i = 2; i <= n; i++) {
         mem[i] = mem[i-1] + mem[i-2]; 
@@ -9,6 +11,9 @@ function fib_memo(n) {
     return mem[n]; 
 }
 
-console.log('fib(4) = ', fib_memo(4)); 
-console.log('Expected = 5'); 
+console.log('fib(4) = ', fib_tab(4)); 
+console.log('Expected = 3'); 
+
+console.log('fib(19) = ', fib_tab(19)); 
+console.log('Expected = 4181'); 
 
